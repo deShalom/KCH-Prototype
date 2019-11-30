a4ae426f57046703e43809b15bc1ce8d78933437
deShalom <jordanunderwoodisepic@gmail.com>
2019-11-30 09:52:17 +0000

    Added Prototype UI & Functionality
    Such as:
    Map
    Buttons
    Text Panels.
    Functioning under a protype GUI management script.

---
+++ KCH Prototype/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour
+{
+    //Variables
+
+    public GameObject monPnl;
+    public Button mon1Btn, tvBtn;
+    public bool monPnlActive;
+
+    //Methods
+    void Start()
+    {
+        monPnl.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OpenmonPNL()
+    {
+        if (monPnlActive == false)
+        {
+            monPnl.SetActive(true);
+            monPnlActive = true;
+        }
+        else if(monPnlActive == true)
+        {
+            monPnl.SetActive(false);
+            monPnlActive = false;
+        }
+    }
+
+}
+
+//All code written by Jay Underwood (deShalom).
