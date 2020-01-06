5294b5375e1a151c6c044375716dc7deb7c85e41
deShalom <jordanunderwoodisepic@gmail.com>
2020-01-06 15:00:05 +0000

    Added backend CH Mon data & UI implementation.
    Added backend CH Mon data & UI implementation.

---
+++ KCH Prototype/Assets/Scripts/UI Population & CH Data/CHMonumentManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+using System.Text;
+
+public class CHMonumentClass
+{
+    [XmlAttribute("BTName")]
+    public string BTNname;
+
+    [XmlElement("CHName")]
+    public string CHMonName;
+    [XmlElement("CHDescription")]
+    public string CHMonDescription;
+    [XmlElement("CHConstructionDat")]
+    public string CHMonConDat;
+    [XmlElement("CHFigures")]
+    public string CHMonFigures;
+    [XmlElement("CHPrice")]
+    public string CHMonPrice;
+    [XmlElement("CHGamepE")]
+    public string CHMonEffect;
+}
+
+[XmlRoot("CHMonumentCollection")]
+public class CHMonumentManager
+{
+
+    //Variables
+    [XmlArray("CHMonuments")]
+    [XmlArrayItem("CHMonument")]
+
+    public List<CHMonumentClass> CHMonumentsList = new List<CHMonumentClass>();
+
+    //Methods
+    public static CHMonumentManager Load(string path)
+    {
+        TextAsset _xml = Resources.Load<TextAsset>(path);
+        XmlSerializer serializer = new XmlSerializer(typeof(CHMonumentManager));
+        StringReader reader = new StringReader(_xml.text);
+        CHMonumentManager CHMonumentsList = serializer.Deserialize(reader) as CHMonumentManager;
+        reader.Close();
+        return CHMonumentsList;
+    }
+}
+//All code written by Jay Underwood (deShalom).
+
+
---
+++ KCH Prototype/Assets/Scripts/UI Population & CH Data/CHMonumentUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+using System.Text;
+public class CHMonumentUI : MonoBehaviour
+{
+    //Variables
+    public const string path = "CHMonData";
+
+    //UI Variables
+    public Text MonTitle, MonDescription, MonPrice, MonEffects;
+
+    //Methods
+    public void Start()
+    {
+      
+    }
+
+    public void StoneHengeClicked()
+    {
+        CHMonumentManager i = CHMonumentManager.Load(path);
+
+        foreach (CHMonumentClass CHMonument in i.CHMonumentsList)
+        {
+            if (CHMonument.BTNname == "CH Monument SH")
+            {
+                MonTitle.text = CHMonument.CHMonName;
+                MonDescription.text = CHMonument.CHMonDescription;
+                MonPrice.text = CHMonument.CHMonPrice;
+                MonEffects.text = CHMonument.CHMonEffect;
+            }
+        }
+    }
+}
+//All code written by Jay Underwood (deShalom).
+
