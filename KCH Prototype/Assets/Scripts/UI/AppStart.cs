c4be84ab2a4b847db6d52cc8a3b2ba2ebaf5f10c
deShalom <jordanunderwoodisepic@gmail.com>
2020-05-18 06:19:23 +0100

    Scene changability!
    Scene changability!

---
+++ KCH Prototype/Assets/Scripts/UI/AppStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppStart : MonoBehaviour
+{
+    public void openGame()
+    {
+        SceneManager.LoadScene("1222");
+    }
+}
